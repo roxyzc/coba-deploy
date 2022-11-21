bc8b40d280d5ece5ef4c83ec6f67176f90ad06b1
roxyzc <roxyzc12@gmail.com>
2022-11-21 18:41:09 +0700

    commit awal

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {res.status(200).json({success: true, message: "Berhasil"})});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening at port ${process.env.PORT}`);
+});
