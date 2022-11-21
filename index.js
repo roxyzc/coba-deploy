71d83d822b6051583fd0f3cb5194cd5acd721534
roxyzc <roxyzc12@gmail.com>
2022-11-21 18:50:48 +0700

    ..

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/api", (req, res) => {res.status(200).json({success: true, message: "Berhasil"})});
+app.get("/", (req, res) => {res.status(200).json({success: true, message: "Coba doang"})})
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening at port ${process.env.PORT}`);
